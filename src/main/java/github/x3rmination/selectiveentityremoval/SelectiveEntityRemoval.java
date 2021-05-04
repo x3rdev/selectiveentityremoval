7cc71a78ff6cbf417a9f487ef5477303c47d1c52
x3rmination <nicolaeungur@gmail.com>
2021-05-04 12:25:22 -0700

    Initial commit

---
+++ src/main/java/github/x3rmination/selectiveentityremoval/SelectiveEntityRemoval.java
@@ -0,0 +1,12 @@
+package github.x3rmination.selectiveentityremoval;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+public class SelectiveEntityRemoval implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}

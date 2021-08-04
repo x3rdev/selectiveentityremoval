11b55113d6991fd2bd316f7fd0f0f0763fa63bb7
x3rmination <nicolaeungur@gmail.com>
2021-08-04 11:44:13 +0300

    Fixes made for the inital release: changed mod settings added entity draw distance check fixed version in modmenu removed extra print message - kacperleague9 changed version format to 1.1.minecraftversion

--- src/main/java/github/x3rmination/selectiveentityremoval/SelectiveEntityRemoval.java
+++ src/main/java/github/x3rmination/selectiveentityremoval/SelectiveEntityRemoval.java
@@ -1,10 +1,18 @@
 package github.x3rmination.selectiveentityremoval;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.MinecraftClient;
 
 public class SelectiveEntityRemoval implements ModInitializer {
 
+	public static final String MOD_ID = "selectiveentityremoval";
+	{ instance = this; }
+	private static SelectiveEntityRemoval instance;
+	public static SelectiveEntityRemoval getInstance() {
+		return instance;
+	}
 
+	private static final MinecraftClient client = MinecraftClient.getInstance();
 
 	@Override
 	public void onInitialize() {
